923e4d1e95961da6deee4df037107bea41cb8ff4
xuyuanxin <xxuyuanxin@163.com>
2014-11-14 16:55:58 +0800

    Create inheritance.cpp

---
+++ language/cpp/inheritance.cpp
@@ -0,0 +1,7 @@
+/*******************************************************************************
+ class D is derived from another class B means that class D has all the features 
+ of class B and some extra, added features as well.This is usually expressed by 
+ saying that the derived class inheritsthe member variables and member functions.
+ B is the base class and D is the derived class. We also say that D is the child 
+ class and B is the parent class.
+ *******************************************************************************/
