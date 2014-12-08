f0b66130a2dfb0b553bd9855d1092b0426863910
xuyuanxin <xxuyuanxin@163.com>
2014-12-08 17:46:53 +0800

    Update lk_list.h

--- project/list/inc/lk_list.h
+++ project/list/inc/lk_list.h
@@ -1,6 +1,6 @@
 
 
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define x_offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 
 
 /**
@@ -12,7 +12,7 @@
  */
 #define container_of(ptr, type, member) ({			\
 	const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
-	(type *)( (char *)__mptr - offsetof(type,member) );})
+	(type *)( (char *)__mptr - x_offsetof(type,member) );})
 
 
 struct list_head 
@@ -37,6 +37,22 @@ static inline void __list_add(struct list_head *new,
 	new->prev = prev;
 	prev->next = new;
 }
+
+
+/*
+ * Delete a list entry by making the prev/next entries
+ * point to each other.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+	next->prev = prev;
+	prev->next = next;
+}
+
+
 
 /**
  * list_entry - get the struct for this entry
@@ -82,15 +98,45 @@ static inline void __list_add(struct list_head *new,
 
 
 /**
+ * list_for_each_entry_safe - iterate over list of given type safe against removal of list entry
+ * @pos:	the type * to use as a loop cursor.
+ * @n:		another type * to use as temporary storage
+ * @head:	the head for your list.
+ * @member: the name of the list_struct within the struct.
+ */
+#define list_for_each_entry_safe(pos, n, head, member)			\
+		for (pos = list_first_entry(head, typeof(*pos), member),	\
+			n = list_next_entry(pos, member);			\
+			 &pos->member != (head);					\
+			 pos = n, n = list_next_entry(n, member))
+
+
+
+
+/**
  * list_add - add a new entry
  * @new: new entry to be added
  * @head: list head to add it after
  *
  * Insert a new entry after the specified head.
  * This is good for implementing stacks.
+
+init
++---+           +---+     +---+         +---+     +---+     +---+
+|   |  add(N1)  | N |---->|   | add(N2) | N |---->| N |---->|   |
+| H |  ------>  |   |     | H | ------> |   |     |   |     | H |
+|   |           | 1 |<----|   |         | 2 |<----| 1 |<----|   |
++---+           +---+     +---+         +---+     +---+     +---+
  */
 static inline void list_add(struct list_head *new, struct list_head *head)
 {
 	__list_add(new, head, head->next);
+}
+
+static inline void list_del(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+	entry->next = NULL/*LIST_POISON1*/;
+	entry->prev = NULL/*LIST_POISON2*/;
 }
 
