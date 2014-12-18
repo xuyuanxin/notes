be2335001cd59ccbc7061b74e727c03bf5e92bd7
xuyuanxin <xxuyuanxin@163.com>
2014-12-18 20:01:42 +0800

    Create linked_list_cycle_II.cpp

---
+++ algocoding/leetcode/linked_list_cycle_II.cpp
@@ -0,0 +1,41 @@
+/*******************************************************************************
+                          Linked List Cycle II(Medium)
+ *******************************************************************************
+ Given a linked list, return the node where the cycle begins. If there is no cycle, 
+ return null.
+ 
+ Follow up:
+ Can you solve it without using extra space?
+ *******************************************************************************
+                             interface
+ *******************************************************************************
+ struct ListNode 
+ {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+ class Solution {
+ public:
+	 ListNode *detectCycle(ListNode *head) {
+		 
+	 }
+ };
+ *******************************************************************************
+                              algo
+ *******************************************************************************
+
+ ******************************************************************************/
+
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+    }
+};
+
+
+
+
