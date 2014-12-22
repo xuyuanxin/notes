caef09e41def171191c2e35090f506d1603ab236
xuyuanxin <xxuyuanxin@163.com>
2014-12-22 19:57:43 +0800

    Create cpp_container_vector.cpp

---
+++ example/cpp/containers/cpp_container_vector.cpp
@@ -0,0 +1,26 @@
+// vector::begin/end
+#include <iostream>
+#include <vector>
+
+/* test begin/end */
+int cpp_vector_test01 ()
+{
+    std::vector<int> myvector;
+	
+    for (int i=1; i<=5; i++) myvector.push_back(i);
+
+    std::cout << "myvector contains:";
+
+	for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
+        std::cout << ' ' << *it;
+	
+    std::cout << '\n';
+
+    return 0;
+}
+
+
+int main()
+{
+    cpp_vector_test01();
+}
