3e8fddad0536033b73554ff04bdd758b84407972
xuyuanxin <xxuyuanxin@163.com>
2014-12-26 18:00:10 +0800

    Update cpp_std_algorithm.cpp

--- algocoding/leetcode/cpp_std_algorithm.cpp
+++ algocoding/leetcode/cpp_std_algorithm.cpp
@@ -1,37 +1,3 @@
-#include <iostream>     // std::cout
-#include <algorithm>    // std::includes, std::sort std::find_if
-#include <vector>       // std::vector
-
-
-
-
-// includes algorithm example
-bool myfunction (int i, int j) { return i<j; }
-
-/*
- Output:
- container includes continent!
- container includes continent!
-*/
-int cpp_algorithm_includes () 
-{
-    int container[] = {5,10,15,20,25,30,35,40,45,50};
-    int continent[] = {40,30,20,10};
-
-    std::sort (container,container+10);
-    std::sort (continent,continent+4);
-
-    // using default comparison:
-    if ( std::includes(container,container+10,continent,continent+4) )
-        std::cout << "container includes continent!\n";
-
-    // using myfunction as comp:
-    if ( std::includes(container,container+10,continent,continent+4, myfunction) )
-        std::cout << "container includes continent!\n";
-
-    return 0;
-}
-
 bool IsOdd (int i) {
   return ((i%2)==1);
 }
@@ -52,6 +18,38 @@ int cpp_algorithm_find_if ()
 
     std::vector<int>::iterator it = std::find_if (myvector.begin(), myvector.end(),IsOdd);
     std::cout << "The first odd value is " << *it << '\n';
+
+    return 0;
+}
+
+
+/************************ transform algorithm example *************************/
+int op_increase (int i) { return ++i; }
+
+/* output:foo contains: 21 41 61 81 101*/
+int cpp_algorithm_transform () 
+{
+    std::vector<int> foo;
+    std::vector<int> bar;
+
+    for (int i=1; i<6; i++)
+        foo.push_back (i*10);  // foo: 10 20 30 40 50
+
+    bar.resize(foo.size());   // allocate space
+
+    std::transform (foo.begin(), foo.end(), bar.begin(), op_increase);
+                                                  // bar: 11 21 31 41 51
+
+    // std::plus adds together its two arguments:
+    std::transform (foo.begin(), foo.end(), bar.begin(), foo.begin(), std::plus<int>());
+                                                  // foo: 21 41 61 81 101
+
+    std::cout << "foo contains:";
+												  
+    for (std::vector<int>::iterator it=foo.begin(); it!=foo.end(); ++it)
+        std::cout << ' ' << *it;
+	
+    std::cout << '\n';
 
     return 0;
 }
