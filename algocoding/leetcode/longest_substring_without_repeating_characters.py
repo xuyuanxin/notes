c8e4eec8eff774b8c825bf0a6e136bb94d8c19e0
xuyuanxin <xxuyuanxin@163.com>
2015-02-03 20:00:15 +0800

    Create longest_substring_without_repeating_characters.py

---
+++ algocoding/leetcode/longest_substring_without_repeating_characters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+#####################################################################################
+#reference http://chaoren.is-programmer.com/posts/42744.html
+#两个指针从左往右扫描, 当头指针遇到重复字母时, 用尾指针找到上一次出现的重复字母，用此
+#时所得子串的长度于更新max, 以尾指针的index+1作为新的搜索起始位置。头指针要扫描到最后
+#一个字母, 别忘了最后得到的子串长度也要更新max。
+class Solution:
+    # @return an integer
+    def lengthOfLongestSubstring(self, s):
+        maxLen = 0
+        subStr = ''
+        rear = 0
+        for front in xrange(len(s)):
+            if s[front] not in subStr:
+                subStr += s[front]
+            else:
+                maxLen = max(maxLen, len(subStr))
+                while s[rear] != s[front]: rear += 1
+                rear += 1
+                subStr = s[rear : front+1] #[rear,front+1)
+        return max(maxLen, len(subStr))
+		
+solu=Solution()
+print solu.lengthOfLongestSubstring('abcdecf')
