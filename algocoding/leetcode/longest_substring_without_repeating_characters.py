b496d773e1236ac4fb0aa429e5f0a88d3faad5f8
xuyuanxin <xxuyuanxin@163.com>
2015-02-11 19:49:49 +0800

    Update longest_substring_without_repeating_characters.py

--- algocoding/leetcode/longest_substring_without_repeating_characters.py
+++ algocoding/leetcode/longest_substring_without_repeating_characters.py
@@ -6,6 +6,38 @@
 #两个指针从左往右扫描, 当头指针遇到重复字母时, 用尾指针找到上一次出现的重复字母，用此
 #时所得子串的长度于更新max, 以尾指针的index+1作为新的搜索起始位置。头指针要扫描到最后
 #一个字母, 别忘了最后得到的子串长度也要更新max。
+# example : "abcdbefchijk"
+#---------------------------
+# rear
+#  |
+#  a b c d b e f c h i j k      ----> len = 4
+#          |
+#          front
+#---------------------------
+#    rear
+#      |
+#  a b c d b e f c h i j k
+#          |
+#          front
+#---------------------------
+#    rear
+#      |
+#  a b c d b e f c h i j k      ----> len = 5
+#                |
+#              front
+#---------------------------
+#      rear
+#        |
+#  a b c d b e f c h i j k
+#                |
+#              front
+#---------------------------
+#      rear
+#        |
+#  a b c d b e f c h i j k      ----> len = 9 (max)
+#                        |
+#                       front
+#---------------------------
 class Solution:
     # @return an integer
     def lengthOfLongestSubstring(self, s):
@@ -23,4 +55,5 @@ class Solution:
         return max(maxLen, len(subStr))
 		
 solu=Solution()
-print solu.lengthOfLongestSubstring('abcdecf')
+
+print solu.lengthOfLongestSubstring('abcdbefchijk')
