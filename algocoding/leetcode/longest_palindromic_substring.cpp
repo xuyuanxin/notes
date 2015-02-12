f79f0bd2228c2369199808b20df7fa7a629770ed
xuyuanxin <xxuyuanxin@163.com>
2015-02-12 17:32:47 +0800

    Update longest_palindromic_substring.cpp

--- algocoding/leetcode/longest_palindromic_substring.cpp
+++ algocoding/leetcode/longest_palindromic_substring.cpp
@@ -30,7 +30,7 @@ public:
 
 #if 0
 /*
-暴力搜索O(N^3)
+Violence searchO(N^3)
 */
 string longestPalindrome_v1(string s) 
 {
@@ -76,9 +76,9 @@ bool isPalindrome(string s)
 #endif
 
 /*-----------------------------------------------------------------------------------
-动态规划
+dynamic programming
 
-定义函数P[i,j]=字符串区间[i,j]是否为palindrome。
+P[i,j]==1,  if string between i,j is palindrome
 1 if(i == j) 
       P[i,j]==1
 2 if(j==i+1) 
@@ -108,77 +108,86 @@ string longestPalindrome_v2(string s)
     return s.substr(start, end-start +1);  
 }
 
-/*-----------------------------------------------------------------------------------
-中心扩展法
-    因为回文字符串是以中心轴对称的，所以如果我们从下标 i 出发，用2个指针向 i 的两边扩
-    展判断是否相等，那么只需要对0到n-1的下标都做此操作，就可以求出最长的回文子串。但需
-    要注意的是，回文字符串有奇偶对称之分，即"abcba"与"abba"2种类型，因此需要在代码编写
-    时都做判断。
-
-http://blog.csdn.net/zhouworld16/article/details/16842467
------------------------------------------------------------------------------------*/
+/* symmetric(N^2) */
 string longestPalindrome_v3(string s) 
 {
     size_t n = s.length();
-    int startPos = 0;
-    int max = 1;
+    int pos = 0;
+    int maxlen = 1,len = 0,beg = 0;;
     for (int i = 0; i < n; ++i) {
-        int oddLen = 0, evenLen = 0, curLen;
-        oddLen = Palindromic(s,i,i);
-        
-        if (i + 1 < n) {
-            evenLen = Palindromic(s,i,i+1);
-        }
-        
-        curLen = oddLen > evenLen? oddLen : evenLen;
-        
-        if (curLen > max) {
-            max = curLen;
-            if (max & 0x1) {
-              startPos = i - max / 2;
-            } else  {
-              startPos = i - (max - 1) / 2;
-            }
-        }
+        len = palindrome_len(s,i,pos);
+		if(len > maxlen) {
+			maxlen = len;
+			beg = pos;
+		}
     }
-    
-    return s.substr(startPos,max);
+    return s.substr(beg,maxlen);
 }
-    
-int Palindromic(const string &str, int i, int j)
+
+/*-----------------------------------------------------------------------------------
+  i==2      
+  |
+cabad        return 3 index==1
+   
+  i==2      
+  |
+cabbad       return 4 index==1
+
+  i==2      
+  |
+abcd         return 1 index==2
+-----------------------------------------------------------------------------------*/
+int palindrome_len(const string &str,int i,int &index)
 {
     size_t n = str.length();
-    int curLen = 0;
+    int len1 = 0,len2 = 0,maxlen = 0,left = 0,right = 0;
 
-    while (i >= 0 && j < n && str[i] == str[j]) {
-		--i;
-        ++j;
+	left  = i-1; right = i+1; /* case: aba */
+    while (left >= 0 && right < n && str[left] == str[right]) {
+		--left;
+        ++right;
     }
-    curLen = (j-1) - (i+1) + 1;
-    return curLen;
-}
+    len1 = (right-1) - (left+1) + 1;
+	index = left+1;
 
+	left  = i; right = i+1; /* case: abba */
+    while (left >= 0 && right < n && str[left] == str[right]) {
+		--left;
+        ++right;
+    }
+    len2 = (right-1) - (left+1) + 1;
+
+	if(len1 > len2) {
+		maxlen = len1;
+	} else {
+	    maxlen = len2;
+		index  = left+1;
+	}
+	
+    return maxlen;
+}
 
 
 };
 
 
-
-
 /*---------------------------------------------------------------------------------*/ 
-
 void longest_palindrome_test(string s)
 {
 	Solution solu;
-	 
-	cout << s << " (V2): " << solu.longestPalindrome_v2(s) << endl;
+
+	cout << "string: " << s << endl;
+	cout << "(V2)  : " << solu.longestPalindrome_v2(s) << endl;
+	cout << "(V3)  : " << solu.longestPalindrome_v3(s) << endl;
 }
  
- 
- int main()
- {	  
-	 longest_palindrome_test("abbac");
-	 longest_palindrome_test("ccabbaabc");
+int main()
+{	  
+    longest_palindrome_test("abbac");
+    longest_palindrome_test("ccabbaabc");
+    longest_palindrome_test("abccba");
+    longest_palindrome_test("aaaaaaaaaaaa");
+    longest_palindrome_test("a");
 
-	 return 0;
- }
+    return 0;
+}
