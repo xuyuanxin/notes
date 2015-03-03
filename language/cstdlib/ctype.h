50c6196db13ed90a97bbeabf2c47da79bc9760d2
xuyuanxin <xxuyuanxin@163.com>
2015-03-03 20:24:38 +0800

    Update ctype.h

--- language/cstdlib/ctype.h
+++ language/cstdlib/ctype.h
@@ -1,5 +1,5 @@
 
-/*
+/*-----------------------------------------------------------------------------------
  @c
     Character to be checked, casted to an int, or EOF.
  @ret
@@ -15,7 +15,20 @@
  '\v' (0x0b)   vertical tab (VT)
  '\f' (0x0c)   feed (FF)
  '\r' (0x0d)   carriage return (CR)
-*/
-
+-----------------------------------------------------------------------------------*/
 int isspace ( int c );
+
+
+/*-----------------------------------------------------------------------------------
+ @c
+    Character to be checked, casted to an int, or EOF.
+ @func
+    Check if character is decimal digit
+ @ret
+    A value different from zero(i.e.,true) if indeed c is a decimal digit. Zero(i.e., 
+    false) otherwise.
+
+ Decimal digits are any of: 0 1 2 3 4 5 6 7 8 9
+-----------------------------------------------------------------------------------*/
+int isdigit ( int c );
 
