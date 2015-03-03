c5fd450aba675f0b3fdedf52d532a40a509d769b
xuyuanxin <xxuyuanxin@163.com>
2015-03-03 20:26:30 +0800

    Update and rename language/cstdlib to language/cstdlib/limits.h

---
+++ language/cstdlib/limits.h
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------------------
+INT_MIN	
+    Minimum value for an object of type int -32767 (-215+1) or less*
+INT_MAX	
+    Maximum value for an object of type int 32767 (215-1) or greater*
+
+ * the actual value depends on the particular system and library implementation , but 
+ shall reflect the limits of these types in the target platform.    
+-----------------------------------------------------------------------------------*/
+#define INT_MIN	
+#define INT_MAX	
