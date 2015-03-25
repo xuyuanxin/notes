aabd2d298a7f639289cae81ed9f77e39e7c9127d
yuanxin <xxuyuanxin@163.com>
2015-03-26 00:09:43 +0800

    m

--- example/linux_shell/sed_lst.sh
+++ example/linux_shell/sed_lst.sh
@@ -1,8 +1,9 @@
-#------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------  
 #http://www.ibm.com/developerworks/library/l-sed1/index.html
 #
 #http://www.ibm.com/developerworks/cn/linux/shell/sed/sed-1/index.html
 #http://www.ibm.com/developerworks/cn/linux/shell/sed/sed-2/index.html
+#http://www.ibm.com/developerworks/cn/linux/shell/sed/sed-3/index.html
 #
 #Sed works by performing any number of user-specified editing operations ("commands") 
 #on the input data. Sed is line-based , so the commands are performed on each line in 
@@ -91,27 +92,87 @@
 #
 #替换！
 #$ sed -e 's/foo/bar/' myfile.txt
+#上面的命令将 myfile.txt 中每行第一次出现的 'foo'（如果有的话）用字符串 'bar' 替换，然
+#后将该文件内容输出到标准输出。在进行字符串替换时，通常想执行全局替换。也就是说，要替
+#换每行中的 所有出现，如下所示： 
+#$ sed -e 's/foo/bar/g' myfile.txt
+#在最后一个斜杠之后附加的 'g' 选项告诉 sed 执行全局替换。
 
-上面的命令将 myfile.txt 中每行第一次出现的 'foo'（如果有的话）用字符串 'bar' 替换，然后将该文件内容输出到标准输出。请注意，我说的是 每行第一次出现，尽管这通常不是您想要的。在进行字符串替换时，通常想执行全局替换。也就是说，要替换每行中的 所有出现，如下所示： 
+#关于 's///' 替换命令，还有其它几件要了解的事。首先，它是一个命令，并且只是一个命令，
+#在所有上例中都没有指定地址。这意味着，'s///' 还可以与地址一起使用来控制要将命令应用到
+#哪些行，如下所示：
+#$ sed -e '1,10s/enchantment/entrapment/g' myfile2.txt
+#上例将导致用短语 'entrapment' 替换所有出现的短语 'enchantment'，但是只在第一到第十行
+#（包括这两行）上这样做。
 
-$ sed -e 's/foo/bar/g' myfile.txt
+#$ sed -e '/^$/,/^END/s/hills/mountains/g' myfile3.txt
+#该例将用 'mountains' 替换 'hills'，但是，只从空行开始，到以三个字符 'END' 开始的行结
+#束（包括这两行）的文本块上这样做。
 
-在最后一个斜杠之后附加的 'g' 选项告诉 sed 执行全局替换。
+#关于 's///' 命令的另一个妙处是 '/' 分隔符有许多替换选项。如果正在执行字符串替换，并且
+#规则表达式或替换字符串中有许多斜杠，则可以通过在 's' 之后指定一个不同的字符来更改分隔
+#符。例如，下例将把所有出现的 /usr/local 替换成 /usr：
+#$ sed -e 's:/usr/local:/usr:g' mylist.txt
+#在该例中，使用冒号作为分隔符。如果需要在规则表达式中指定分隔符字符，可以在它前面加入
+#反斜杠。
 
-关于 's///' 替换命令，还有其它几件要了解的事。首先，它是一个命令，并且只是一个命令，在所有上例中都没有指定地址。这意味着，'s///' 还可以与地址一起使用来控制要将命令应用到哪些行，如下所示：
+#以下 sed 命令将匹配从 '<' 开始、到 '>' 结束、并且在其中包含任意数量字符的短语。下例将
+#删除该短语（用空字符串替换）：
+#$ sed -e 's/<.*>//g' myfile.html
+#这意味着，上例将把下行：
+#<b>This</b> is what <b>I</b> meant.
+#变成：
+#meant.
 
- $ sed -e '1,10s/enchantment/entrapment/g' myfile2.txt
+#'<' 字符后面跟有任意数量非 '>' 字符并以 '>' 字符结束，的规则表达式。这将与最短、而不是
+#最长的可能性匹配
+#$ sed -e 's/<[^>]*>//g' myfile.html
+#这意味着，上例将把下行：
+#<b>This</b> is what <b>I</b> meant.
+#变成：
+#This is what I meant.
+#在上例中，'[^>]' 指定“非 '>'”字符，其后的 '*' 完成该表达式以表示“零或多个非 '>' 字符”。
 
-上例将导致用短语 'entrapment' 替换所有出现的短语 'enchantment'，但是只在第一到第十行（包括这两行）上这样做。
+#以下是可用字符类的相当完整的列表：
+#字符类	    描述
+#[:alnum:]	字母数字 [a-z A-Z 0-9]
+#[:alpha:]	字母 [a-z A-Z]
+#[:blank:]	空格或制表键
+#[:cntrl:]	任何控制字符
+#[:digit:]	数字 [0-9]
+#[:graph:]	任何可视字符（无空格）
+#[:lower:]	小写 [a-z]
+#[:print:]	非控制字符
+#[:punct:]	标点字符
+#[:space:]	空格
+#[:upper:]	大写 [A-Z]
+#[:xdigit:]	十六进制数字 [0-9 a-f A-F]
+#尽可能使用字符类是很有利的，因为它们可以更好地适应非英语 locale（包括某些必需的重音字
+#符等等）.
 
- $ sed -e '/^$/,/^END/s/hills/mountains/g' myfile3.txt
+#cat a.txt
+#ab cd
+#aa
+# bb
+#$ sed -e 's/.*/ralph said: &/' a.txt
+#ralph said: ab cd
+#ralph said: aa
+#ralph said:  bb
+#该例的替换字符串中使用了 '&' 字符，该字符告诉 sed 插入整个匹配的规则表达式。
 
-该例将用 'mountains' 替换 'hills'，但是，只从空行开始，到以三个字符 'END' 开始的行结束（包括这两行）的文本块上这样做。
-
-关于 's///' 命令的另一个妙处是 '/' 分隔符有许多替换选项。如果正在执行字符串替换，并且规则表达式或替换字符串中有许多斜杠，则可以通过在 's' 之后指定一个不同的字符来更改分隔符。例如，下例将把所有出现的 /usr/local 替换成 /usr：
-
- $ sed -e 's:/usr/local:/usr:g' mylist.txt
-
-在该例中，使用冒号作为分隔符。如果需要在规则表达式中指定分隔符字符，可以在它前面加入反斜杠。
+#'s///' 允许我们在规则表达式中定义区域，然后可以在替换字符串中引用这些特定区域。作为示
+#例，假设有一个包含以下文本的文件：
+# foo bar oni eeny meeny miny larry curly moe 
+#现在假设要编写一个 sed 脚本，该脚本将把 "eeny meeny miny" 替换成 "Victor eeny-meeny Von
+# miny" 等等。要这样做，首先要编写一个由空格分隔并与三个字符串匹配的规则表达式。
+# '.* .* .*'
+#现在，将在其中每个感兴趣的区域两边插入带反斜杠的圆括号来定义区域：
+# '\(.*\) \(.*\) \(.*\)'
+#除了要定义三个可在替换字符串中引用的逻辑区域以外，该规则表达式的工作原理将与第一个规则
+#表达式相同。下面是最终脚本：
+# $ sed -e 's/\(.*\) \(.*\) \(.*\)/Victor \1-\2 Von \3/' myfile.txt
+#如您所见，通过输入 '\x'（其中，x 是从 1 开始的区域号）来引用每个由圆括号定界的区域。输
+#入如下：
+#Victor foo-bar Von oni Victor eeny-meeny Von miny Victor larry-curly Von moe
 
 
