d83ef7dae85a41ec8e86f7aeec16ca0a91523455
yuanxin <xxuyuanxin@163.com>
2015-05-18 23:05:46 +0800

    m

--- example/java/class_java/ConstructorTest.java
+++ example/java/class_java/ConstructorTest.java
@@ -1,10 +1,5 @@
 import java.util.*;
 
-/**
- * This program demonstrates object construction.
- * @version 1.01 2004-02-19
- * @author Cay Horstmann
- */
 public class ConstructorTest
 {
    public static void main(String[] args)
@@ -31,36 +26,6 @@ class Employee
    private String name = ""; // instance field initialization
    private double salary;
 
-
-/*
-initialization block
-    By assigning a value in the declaration There is a third mechanism in Java,called 
-    an initialization block. Class declarations can contain arbitrary blocks of code. 
-    These blocks are executed whenever an object of that class is constructed. The i-
-    nitialization block runs first, and then the body of the constructor is executed.
-
-    If the static fields of your class require complex initialization code, use a st-
-    atic initialization block. Place the code inside a block and tag it with the key-
-    word static. Static initialization occurs when the class is first loaded. Like i-
-    nstance fields, static fields are 0, false, or null unless you explicitly set th-
-    em to another value. All static field initializers and static initialization blo-
-    cks are executed in the order in which they occur in the class declaration. 
-
-    Here is a Java trivia fact to amaze your fellow Java coders: You can write a "He-
-    llo, World" program in Java without ever writing a main method.
-				public class Hello
-				{
-				   static
-				   {
-				      System.out.println("Hello, World");
-				   }
-				}
-    When you invoke the class with java Hello, the class is loaded, the static initi-
-    alization block prints "Hello, World", and only then do you get an ugly error me-
-    ssage that main is not defined. You can avoid that blemish by calling System.exit(0) 
-    at the end of the static initialization block.
-*/
-  
    // static initialization block
    static
    {
@@ -114,110 +79,127 @@ initialization block
 
 
 
-/*
-Overloading
-    Overloading occurs if several methods have the same name but different parameter-
-    s. The compiler must sort out which method to call. It picks the correct method -
-    by matching the parameter types in the headers of the various methods with the t-
-    ypes of the values used in the specific method call . A compile-time error occurs  
-    if the compiler cannot match the parameters or if more than one match is possible. 
-    (This process is called overloading resolution.)
-signature of the method
-    Thus, to completely describe a method, you need to specify its name together with  
-    its parameter types. This is called the signature of the method. For example, the 
-    String class has four public methods called indexOf. They have signatures
-				indexOf(int)
-				indexOf(int, int)
-				indexOf(String)
-				indexOf(String, int)
-    The return type is not part of the method signature. That is, you cannot have two 
-    methods with the same names and parameter types but different return types.
+/*-----------------------------------------------------------------------------------
+ ----> Overloading
+ Overloading occurs if several methods have the same name but different parameters. -
+ The compiler must sort out which method to call. It picks the correct method by mat-
+ ching the parameter types in the headers of the various methods with the types of t-
+ he values used in the specific method call. A compile-time error occurs if the comp-
+ iler cannot match the parameters or if more than one match is possible. (This proce-
+ ss is called overloading resolution.)
+ 
+ signature of the method
+ Thus, to completely describe a method, you need to specify its name together with i-
+ ts parameter types. This is called the signature of the method. For example, the St-
+ ring class has four public methods called indexOf. They have signatures
+     indexOf(int)
+     indexOf(int, int)
+     indexOf(String)
+     indexOf(String, int)
+ The return type is not part of the method signature. That is, you cannot have two m-
+ ethods with the same names and parameter types but different return types.
 
-Object Construction
-1 Default Field Initialization
-    If you don't set a field explicitly in a constructor , it is automatically set to 
-    a default value:numbers to 0,boolean values to false,and object references to null.
-2 The Constructor with No Arguments
-    If you write a class with no constructors whatsoever, then a no-argument constru-
-    ctor is provided for you . This constructor sets all the instance fields to their 
-    default values. Please keep in mind that you get a free no-argument constructor -
-    only when your class has no other constructors . If a class supplies at least one 
-    constructor but does not supply a no-argument constructor, it is illegal to cons-
-    truct objects without supplying arguments.
-3 Explicit Field Initialization
-    It is always a good idea to make sure that, regardless of the constructor call, -
-    every instance field is set to something meaningful. You can simply assign a val-
-    ue to any field in the class definition. For example:
-				class Employee
-				{
-				   private String name = "";
-				   . . .
-				}
-    This assignment is carried out before the constructor executes. This syntax is p-
-    articularly useful if all constructors of a class need to set a particular insta-
-    nce field to the same value. The initialization value doesn't have to be a const-
-    ant value. Here is an example in which a field is initialized with a method call. 
-    Consider an Employee class where each employee has an id field. You can initiali-
-    ze it as follows:
-				class Employee
-				{
-				   private static int nextId;
-				   private int id = assignId();
-				   . . .
-				   private static int assignId()
-				   {
-				      int r = nextId;
-				      nextId++;
-				      return r;
-				   }
-				   . . .
-				}
-4  Calling Another Constructor
-    The keyword this refers to the implicit parameter of a method. However, this key-
-    word  has  a second meaning. If the first statement of a constructor has the form 
-    this(. . .), then the constructor calls another constructor of the same class. H-
-    ere is a typical example:
-				public Employee(double s)
-				{
-				   // calls Employee(String, double)
-				   this("Employee #" + nextId, s);
-				   nextId++;
-				}
-    When you call new Employee(60000), the Employee(double) constructor calls the
-    Employee(String, double) constructor. Using the this keyword in this manner is u-
-    seful, you only need to write common construction code once.
+ ----> Object Construction
+ 1 Default Field Initialization
+ If you don't set a field explicitly in a constructor, it is automatically set to a -
+ default value:numbers to 0,boolean values to false,and object references to null.
 
-Object Destruction and the finalize Method
-    Since Java does automatic garbage collection, manual memory reclamation is not n-
-    eeded, so Java does not support destructors. Of course, some objects utilize a r-
-    esource other than memory, such as a file or a handle to another object that uses  
-    system  resources. In this case, it is important that the resource be reclaimed -
-    and recycled when it is no longer needed. You can add a finalize method to any c-
-    lass. The finalize method will be called before the garbage collector sweeps away  
-    the object. In practice, do not rely on the finalize method for recycling any re-
-    sources that are  in short supply you simply cannot know when this method will be
-    called.
+ 2 The Constructor with No Arguments
+ If you write a class with no constructors whatsoever, then a no-argument constructor 
+ is provided for you . This constructor sets all the instance fields to their default 
+ values. Please keep in mind that you get a free no-argument constructor only when y-
+ our class has no other constructors. If a class supplies at least one constructor b-
+ ut does not supply a no-argument constructor, it is illegal to construct objects wi-
+ thout supplying arguments.
 
-Final Instance Fields
-    You can define an instance field as final. Such a field must be initialized  when 
-    the object is constructed. That is, you must guarantee that the field value has -
-    been set after the end of every constructor. Afterwards, the field may not be mo-
-    dified again. For example, the name field of the Employee class may be declared -
-    as final because it never changes after the object is constructed.
-            class Employee
-            {
-                   private final String name;
-                   . . .
-            }
-    The final modifier is particularly useful for fields whose type is primitive or -
-    an immutable class. ( A class is immutable if none of its methods ever mutate its 
-    objects. For example, the String class is immutable.) For mutable classes, the f-
-    inal modifier is likely to confuse the reader. For example,
-            private final Date hiredate;
-    merely means that the object reference stored in the hiredate variable doesn't g-
-    et changed after the object is constructed . That does not mean that the hiredate 
-    object is constant.Any method is free to invoke the setTime mutator on the object 
-    to which hiredate refers.
+ 3 Explicit Field Initialization
+ It is always a good idea to make sure that, regardless of the constructor call, eve-
+ ry instance field is set to something meaningful. You can simply assign a value to -
+ any field in the class definition. For example:
+     class Employee
+     {
+         private String name = "";
+         . . .
+     }
+ This assignment is carried out before the constructor executes. This syntax is part-
+ icularly useful if all constructors of a class need to set a particular instance fi-
+ eld to the same value. The initialization value doesn't have to be a constant value. 
+ Here is an example in which a field is initialized with a method call. Consider an -
+ Employee class where each employee has an id field.You can initialize it as follows:
+	class Employee
+	{
+	   private static int nextId;
+	   private int id = assignId();
+	   . . .
+	   private static int assignId()
+	   {
+	      int r = nextId;
+	      nextId++;
+	      return r;
+	   }
+	   . . .
+	}
+
+ 4 Calling Another Constructor
+ The keyword @this refers to the implicit parameter of a method. However, this keywo-
+ rd has a second meaning. If the first statement of a constructor has the form this(. . .), 
+ then the constructor calls another constructor of the same class . Here is a typical 
+ example:
+	public Employee(double s)
+	{
+	   this("Employee #" + nextId, s); // calls Employee(String, double)
+	   nextId++;
+	}
+ When you call new Employee(60000), the Employee(double) constructor calls the 
+ Employee(String, double) constructor. Using the this keyword in this manner is usef-
+ ul, you only need to write common construction code once.
+
+ 5 initialization block
+ By assigning a value in the declaration There is a third mechanism in Java, called -
+ an initialization block. Class declarations can contain arbitrary blocks of code. T-
+ hese blocks are executed whenever an object of that class is constructed. The initi-
+ alization block runs first, and then the body of the constructor is executed.
+
+ If the static fields of your class require complex initialization code, use a static 
+ initialization block. Place the code inside a block and tag it with the keyword sta-
+ tic. Static initialization occurs when the class is first loaded. Like instance fie-
+ lds, static fields are 0, false, or null unless you explicitly set them to another -
+ value. All static field initializers and static initialization blocks are executed -
+ in the order in which they occur in the class declaration. 
+
+ Here is a Java trivia fact to amaze your fellow Java coders: You can write a "Hello, 
+ World" program in Java without ever writing a main method.
+	public class Hello
+	{
+	   static
+	   {
+	      System.out.println("Hello, World");
+	   }
+	}
+ When you invoke the class with 
+     java Hello
+ the class is loaded, the static initialization block prints "Hello, World", and onl-
+ y then do you get an ugly error message that main is not defined. You can avoid that 
+ blemish by calling System.exit(0) at the end of the static initialization block.
+
+ With so many ways of initializing data fields, it can be quite confusing to give all
+ possible pathways for the construction process . Here is what happens in detail when 
+ a constructor is called:
+ 1. All data fields are initialized to their default values (0, false, or null).
+ 2. All field initializers and initialization blocks are executed, in the order in w-
+    hich they occur in the class declaration.
+ 3. If the first line of the constructor calls a second constructor, then the body of
+    the second constructor is executed.
+ 4. The body of the constructor is executed.
 
 
-*/
+ ----> Object Destruction and the finalize Method
+ Since Java does automatic garbage collection, manual memory reclamation is not need-
+ ed, so Java does not support destructors. Of course, some objects utilize a resource 
+ other than memory, such as a file or a handle to another object that uses system re-
+ sources. In this case, it is important that the resource be reclaimed and recycled -
+ when it is no longer needed. You can add a finalize method to any class. The finali-
+ ze method will be called before the garbage collector sweeps away the object. In pr-
+ actice, do not rely on the finalize method for recycling any resources that are in -
+ short supply you simply cannot know when this method will be called.
+ ----------------------------------------------------------------------------------*/
--- example/java/class_java/ParamTest.java
+++ example/java/class_java/ParamTest.java
@@ -1,8 +1,4 @@
-/**
- * This program demonstrates parameter passing in Java.
- * @version 1.00 2000-01-27
- * @author Cay Horstmann
- */
+
 public class ParamTest
 {
    public static void main(String[] args)
@@ -43,13 +39,6 @@ public class ParamTest
       x = 3 * x;
       System.out.println("End of method: x=" + x);
    }
-
-/*-----------------------------------------------------------------------------------
- There are,however, two kinds of method parameters: Primitive types (numbers, boolean 
- values)，Object references. You have seen that it is impossible for a method to cha-
- nge a primitive type parameter. The situation is different for object parameters. Y-
- ou can easily implement a method that triples the salary of an employee:tripleSalary
------------------------------------------------------------------------------------*/
    public static void tripleSalary(Employee x) // works
    {
       x.raiseSalary(200);
@@ -97,13 +86,87 @@ class Employee // simplified Employee class
 
 
 /*-----------------------------------------------------------------------------------
-Method Parameters
-    The Java programming language always uses call by value. That means that the met-
-    hod gets a copy of all parameter values. In  particular, the method cannot modify  
-    the contents of any parameter variables passed to it.
-    
-Here is a summary of what you can and cannot do with method parameters in Java:
-    A method cannot modify a parameter of a primitive type (that is, numbers or boolean values).
-    A method can change the state of an object parameter.
-    A method cannot make an object parameter refer to a new object.
------------------------------------------------------------------------------------*/
+ ----> call by value, call by reference
+ Let us review the computer science terms that describe how parameters can be  passed 
+ to a method (or a function) in a programming language . The term call by value means 
+ that the method gets just the value that the caller provides. In contrast, call by -
+ reference means that the method gets the location of the variable that the caller p-
+ rovides. Thus,a method can modify the value stored in a variable passed by reference
+ but not in one passed by value.
+ 
+ The Java programming language always uses call by value . That means that the method 
+ gets a copy of all parameter values. In particular, the method cannot modify the co-
+ ntents of any parameter variables passed to it. For example , consider the following 
+ call:
+     double percent = 10;
+     harry.raiseSalary(percent);
+ No matter how the method is implemented, we know that after the method call, the va-
+ lue of percent is still 10. Let us look a little more closely at this situation. Su-
+ ppose a method tried to triple the value of a method parameter:
+     public static void tripleValue(double x) // doesn't work
+     {
+         x = 3 * x;
+     }
+ Let's call this method:
+     double percent = 10;
+     tripleValue(percent);
+ However, this does not work. After the method call,the value of percent is still 10.
+ Here is what happens:
+ 1. x is initialized with a copy of the value of percent (that is, 10).
+ 2. x is tripled. it is now 30. But percent is still 10 .
+ 3. The method ends, and the parameter variable x is no longer in use.
+
+ There are, however, two kinds of method parameters: Primitive types (numbers, boole-
+ an values), Object references. You have seen that it is impossible for a method to -
+ change a primitive type parameter. The situation is different for object parameters. 
+ You can easily implement a method that triples the salary of an employee:
+     public static void tripleSalary(Employee x) // works
+     {
+         x.raiseSalary(200);
+     }
+ When you call
+     harry = new Employee(. . .);
+     tripleSalary(harry);
+ then the following happens:
+ 1. x is initialized with a copy of the value of harry, that is, an object reference.
+ 2. The raiseSalary method is applied to that object reference. The Employee object -
+    to which both x and harry refer gets its salary raised by 200 percent.
+ 3. The method ends, and the parameter variable x is no longer in use. Of course, th-
+    e object variable harry continues to refer to the object whose salary was tripled.
+ The reason is simple. The method gets a copy of the object reference, and both the -
+ original and the copy refer to the same object.
+
+ Let's try to write a method that swaps two employee objects:
+     public static void swap(Employee x, Employee y) // doesn't work
+     {
+         Employee temp = x;
+         x = y;
+         y = temp;
+     }
+ If the Java programming language used call by reference for objects, this method wo-
+ uld work:
+     Employee a = new Employee("Alice", . . .);
+     Employee b = new Employee("Bob", . . .);
+     swap(a, b);
+     // does a now refer to Bob, b to Alice?
+ However, the method does not actually change the object references that are stored -
+ in the variables a and b . The x and y parameters of the swap method are initialized 
+ with copies of these references. The method then proceeds to swap these copies.
+     // x refers to Alice, y to Bob
+     Employee temp = x;
+     x = y;
+     y = temp;
+     // now x refers to Bob, y to Alice
+ But ultimately, this is a wasted effort. When the method ends, the parameter variab-
+ les x and y are abandoned. The original variables a and b still refer to the same o-
+ bjects as they did before the method call.
+
+ This demonstrates that the Java programming language  does not use call by reference 
+ for objects. Instead, object references are passed by value.
+
+ Here is a summary of what you can and cannot do with method parameters in Java:
+ 1. A method cannot modify a parameter of a primitive type (that is, numbers or bool-
+    ean values).
+ 2. A method can change the state of an object parameter.
+ 3. A method cannot make an object parameter refer to a new object.
+ ----------------------------------------------------------------------------------*/
